698da013fe81f8d304c68b069d9614b80909b0f1
abdulrahmanibrahimdawood <abdoibrahim9111@gmail.com>
2024-10-04 15:14:01 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/birth_day_card/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.birth_day_card
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Birth_Day());
+}
+
+class Birth_Day extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Color(0XFFD2BCD5),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/birth_day.png'),
+          ),
+        ),
+      ),
+    );
+  }
+}
